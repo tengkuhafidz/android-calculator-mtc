92487880fd190852e0dad43fa28560392a57ec56
tengkuhafidz <tengkuhafidz@gmail.com>
2017-06-04 12:55:38 +0800

    initial commit

---
+++ app/src/main/java/com/tengkuhafidz/calculatorbest/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tengkuhafidz.calculatorbest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.logging.LoggingMXBean;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
